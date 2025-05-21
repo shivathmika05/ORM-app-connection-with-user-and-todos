dc67d5b52ae68d6d45f1b056973ba9c49952d219
shivathmika05 <sivathmikanalanagula2004@gmail.com>
2025-05-21 17:07:18 +0530

    Create usermodel.js

---
+++ usermodel.js
@@ -0,0 +1,27 @@
+const { DataTypes } = require('sequelize');
+const sequelize = require('./db'); // adjust this path to your Sequelize instance
+
+const User = sequelize.define('User', {
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  username: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  email: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    unique: true,
+    validate: {
+      isEmail: true,
+    },
+  },
+}, {
+  tableName: 'users',
+  timestamps: true,
+});
+
+module.exports = User;
